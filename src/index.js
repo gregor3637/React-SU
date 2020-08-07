5bc5ca2b9c74a9a29bc15d57ca175850f9bc04d0
Misho <mihailm956@gmail.com>
2020-08-07 23:40:30 +0300

    fix: renamed files again to initial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Application from './application';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Application />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
